b8a2ae335faeee3487c691ba08882c1003ae7bd9
Mario Ruano Fernánez <mr@marioruano.com>
2017-07-14 13:46:32 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 PlatziMusic
+    h2 Top Artists in:
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" :value="country.value") {{ country.name }}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+    p
+      | Data from 
+      a(href="https://www.last.fm/api" target="_blank") Last.fm
+      |  & coded by      
+      a(href="https://twitter.com/mrf1989" target="_blank") @mrf1989
+</template>
+
+<script>
+import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
+import getArtists from './api'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+        { name: 'Argentina', value: 'argentina' },
+        { name: 'Colombia', value: 'colombia' },
+        { name: 'España', value: 'spain' },
+        { name: 'Ecuador', value: 'ecuador' },
+        { name: 'México', value: 'mexico' }
+      ],
+      selectedCountry: 'spain',
+      loading: true
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  methods: {
+    refreshArtists() {
+      const self = this
+      this.loading = true
+      this.artists = []
+      getArtists(this.selectedCountry)
+        .then(function (artists) {
+          self.loading = false
+          self.artists = artists
+        })
+    }
+  },
+  created: function () {
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry: function () {
+      this.refreshArtists()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+
+p
+  text-align center
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appname: 'platzimusic',
+  apiKey: '162e0d9af96fea1fda0f58c0b95e08c1',
+  secret: 'd52d087b4f3fc7c6856afcb4bc258281',
+  registeredTo: 'mrf1989'
+}
